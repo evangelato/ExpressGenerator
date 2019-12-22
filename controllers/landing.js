749cdc570eafc6c43f97dce7a1bc5c5994efb77e
Evan Hong <evangelurtz@gmail.com>
2019-12-22 16:22:40 +0900

    Add sequelize

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,3 +1,8 @@
 exports.getLanding = function(req, res, next) {
     res.render('landing', { title: 'Express' });
 }
+
+exports.submitLead = function(req, res, next) {
+    console.log("Lead Email: ", req.body.leadEmail);
+    res.redirect('/');
+}
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 let landing = require('../controllers/landing');
 /* GET home page. */
 router.get('/', landing.getLanding);
+router.post('/', landing.submitLead);
 
 module.exports = router;
